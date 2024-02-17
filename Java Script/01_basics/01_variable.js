384afaf392eb08b84b3b4963a2a6f12585cfd0a3
Chirag Rathod <124911968+ChiragRathod25@users.noreply.github.com>
2024-02-17 19:26:06 +0000

    variable

---
+++ Java Script/01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId=374121  //can't modify it further
+let accountEmail = "chiragrathod21@google.com"   //prefer let over var
+var accountPassword=3721 //outdated approach to declare variable
+accountCity="Anand"
+let accountState;  //default it is undefined
+
+//accountId=4566;  //can't modify because declared as const
+
+accountPassword="CAS"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
