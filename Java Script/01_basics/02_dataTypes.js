fddbc20df175980a9b15e28a7bf9eab004f12bd2
Chirag Rathod <124911968+ChiragRathod25@users.noreply.github.com>
2024-02-21 12:42:43 +0000

    new

--- Java Script/01_basics/02_dataTypes.js
+++ Java Script/01_basics/02_dataTypes.js
@@ -16,8 +16,10 @@ let played =true
 //undefined ->not defined , it means value is not defined
 //symbol ->to find uniqueness //mostly for react,figma,etc
 
+
 console.log(typeof(Myname))
 console.log(typeof(age))
 console.log(typeof(played))
 console.log(typeof null)  //object
 console.log(typeof undefined) //undefined
+
