69f4c843c9ec8f9bdab962e38f9000fd938b8983
Chirag Rathod <124911968+ChiragRathod25@users.noreply.github.com>
2024-02-22 18:41:35 +0000

    nums

---
+++ Java Script/01_basics/06_nums_and_maths.js
@@ -0,0 +1,17 @@
+const score=400 //here data type is given by JS
+console.log(score) 
+
+const balance=new Number(100) //define data type to balance(number)
+console.log(balance)
+console.log(balance.toString().length) //it is converted into string and can access methods of string
+console.log(balance.toFixed(2)) //for precision-value after point (.)
+
+const otherNumber=2223.43212343
+console.log(otherNumber.toPrecision(3)) //total output number (before+after)
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString()) //1,000,000
+console.log(hundreds.toLocaleString('en-IN')) //10,00,000
+
+
+// +++++++++++++MATHS+++++++++++++++
