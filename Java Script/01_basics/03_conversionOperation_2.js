2ee72fc0735bc94387d18ede0104694ab57a545d
Chirag Rathod <124911968+ChiragRathod25@users.noreply.github.com>
2024-02-22 12:41:05 +0000

    comparion

---
+++ Java Script/01_basics/03_conversionOperation_2.js
@@ -0,0 +1,39 @@
+// --Operations--
+let value=3;
+let negValue=-value
+// console.log(value)
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);   //2^3
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1="Chirag "
+// let str2="Rathod"
+// let str=str1+str2
+// console.log(str)  //string concate
+
+
+// console.log("1"+2) //converted to '12'
+// console.log("1"+2+3)    //converted to '123'
+// console.log(1+2+"3")    //converted to 33
+
+// console.log(3+4*5%3)
+// console.log((3+4)*(5%3))  //use brackets for proper logic
+
+// console.log(true)  //ouput true
+// console.log(+true)  //true is converted to 1 because of +
+// console.log(-true)  //true is converted to -1 because of -  ; *,/ is not allowed
+// console.log(true+)  //post + and - is not allowed, it'll give error
+
+// console.log(+"") //oputput zero; blank string is converted to zero
+
+// let num1,num2,num3
+// num1=num2=num3=100 // it is correct but it is bad logic
+
+let gameCounter =100
+++gameCounter   //increment and decrement will work as it is
+console.log(gameCounter)
---
+++ Java Script/01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2">1) //it will convert string 2 to number 2 and  then compare it with 1
+// console.log("02"<1) 
+
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+// here == , >= , > operator work differently , here comparision operator convert null to zero and then check condition and == will not convert it to zero 
+
+// console.log(undefined==0)
+// console.log(undefined<0)
+// console.log(undefined>0) //it will always undefined
+
+// === "strinct check"
+console.log("2"===2) // it will give error because data type is not same
