5fd17ea8df43d3c77cd19480032920471dee892b
Chirag Rathod <124911968+ChiragRathod25@users.noreply.github.com>
2024-02-26 04:28:16 +0000

    experiment-5-6_update

--- Labwork/19-02 exp 6/5array.js
+++ Labwork/19-02 exp 6/5array.js
@@ -1,23 +1,18 @@
-function display(arr){
-    for(var i=0;i<arr.length;i++){
-    document.writeln(arr[i]," ")
-    }
-}
 
 var friends=["Khushii",6,"Himanshu","Tulsi"]
-document.writeln("Original Array : [")
-display(friends)
-document.writeln("]","</br>")
+var country=["India","Russia"]
+document.writeln("Original Array : [",friends,"]","</br>")
 
 friends.reverse()
-document.writeln("Reversed Array : [")
-display(friends)
-document.writeln("]","</br>")
+document.writeln("Reversed Array : [",friends,"]","</br>")
 
 friends.sort()
-document.writeln("Sorted Array : [")
-display(friends)
-document.writeln("]","</br>")
+document.writeln("Sorted Array : [",friends,"]","</br>")
 
-var country=["India","Russia"]
-display(country)
+const concats_array=friends.concat(country) 
+//concate method will concate two arrays and return a new array
+document.writeln("Concateed Array :[" ,concats_array,"]","</br>")
+
+const new_array=friends.join("-")
+//join method will convert all array element into string and  join all of them by '-'
+document.writeln("Join : ",new_array)
