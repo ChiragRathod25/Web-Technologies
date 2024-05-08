2f18387675fdac0f4ea13dc5c4873a379ec70751
Chirag Rathod <chiragrathod9987@gmail.com>
2024-05-08 20:58:42 +0530

    update

--- Part_5_PHP/Chap18/split.php
+++ Part_5_PHP/Chap18/split.php
@@ -1,7 +1,6 @@
 <?php
-		$str = "http://www.ivanbayross.com?txtName=Ivan
-						&txtSurname=Bayross&txtDOB=23/06/1952";
-		$columns = spliti(".txt", $str, 4);
-		echo "The parameters and their corresponding values passed are: <br />";
-	print_r($columns);
-	?>
+	$str = "http://www.ivanbayross.com?txtName=Ivan&txtSurname=Bayross&txtDOB=23/06/1952";
+	$parameters = preg_split("/[&=]/", $str);
+	echo "The parameters and their corresponding values passed are: <br />";
+	print_r($parameters);
+?>
